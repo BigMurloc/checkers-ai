81c0b652185074c12a8b2cf6e62ebcf46ade92e0
BigMurloc <s21159@pjwstk.edu.pl>
2023-10-08 08:25:00 +0200

    init

---
+++ src/main/java/pl/bigmurloc/checkersai/CheckersAiApplication.java
@@ -0,0 +1,13 @@
+package pl.bigmurloc.checkersai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckersAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckersAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bigmurloc/checkersai/CheckersAiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bigmurloc.checkersai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckersAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
